5c8d90272adf96b7167afcb8266147918b90d26d
Peter Hanley <hanley.peter@gmail.com>
2022-05-18 10:31:29 +1000

    Changed site title, favicon etc.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Peter Hanley`,
+    siteTitleAlt: `Peter Hanley - Front-End Developer`,
+    siteHeadline: `Peter Hanley - Front-End Developer`,
+    siteUrl: `https://peterhanley.dev`,
+    siteDescription: `Hi, I'm Peter! A Front-End Developer from Melbourne, Australia`,
     siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@peterbrandon`,
   },
   plugins: [
     {
