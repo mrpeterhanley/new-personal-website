91c10fd4b1f37a69a80cf98b72edd90444e539a9
Peter Hanley <hanley.peter@gmail.com>
2022-05-18 16:57:03 +1000

    First version completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,5 +56,34 @@ module.exports = {
         openAnalyzer: false,
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+          resolve: 'gatsby-remark-emojis',
+          options: {
+            // Deactivate the plugin globally (default: true)
+            active : true,
+            // Add a custom css class
+            class  : 'emoji-icon',
+            // In order to avoid pattern mismatch you can specify
+            // an escape character which will be prepended to the
+            // actual pattern (e.g. `#:poop:`).
+            escapeCharacter : '#', // (default: '')
+            // Select the size (available size: 16, 24, 32, 64)
+            size   : 64,
+            // Add custom styles
+            styles : {
+              display      : 'inline',
+              margin       : '0',
+              'margin-top' : '1px',
+              position     : 'relative',
+              top          : '5px',
+              width        : '25px'
+            }
+          }
+      }]
+      }
+    },
   ].filter(Boolean),
 }
